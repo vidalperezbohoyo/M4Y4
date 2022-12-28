0bd079daa95cafa6aaf73f5ee018f56b100a3f41
patricia241 <p.colmenar.2019@alumnos.urjc.es>
2022-12-28 19:42:19 +0100

    maya_pkg

---
+++ software/ros_ws/src/maya_pkg/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'maya_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='patricia',
+    maintainer_email='p.colmenar.2019@alumnos.urjc.es',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'mayaDriver = maya_pkg.mayaDriver:main'
+        ],
+    },
+)
