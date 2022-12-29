8423c0b1ab558cfee6ab1095c57a26a5d74e45f0
patricia241 <p.colmenar.2019@alumnos.urjc.es>
2022-12-29 19:29:08 +0100

    ros2 mayaDrive builded

--- software/ros_ws/src/maya_pkg/setup.py
+++ software/ros_ws/src/maya_pkg/setup.py
@@ -6,6 +6,9 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
+    py_modules=[
+        'maya_pkg.maya_lib.maya'
+    ],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
